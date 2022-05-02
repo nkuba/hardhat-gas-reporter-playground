c980b5c417018e6129ed73166a3f9ca3018b0490
Jakub Nowakowski <jakub.nowakowski@akena.co>
2022-05-02 12:06:09 +0200

    Simplify example to just use ethers

--- deploy/deploy_test_contract.ts
+++ deploy/deploy_test_contract.ts
@@ -1,20 +1,11 @@
 import type { HardhatRuntimeEnvironment } from "hardhat/types"
 import type { DeployFunction } from "hardhat-deploy/types"
-import { writeFileSync } from "fs"
 
 const func: DeployFunction = async (hre: HardhatRuntimeEnvironment) => {
-  const { helpers, upgrades, ethers } = hre
+  const { ethers } = hre
 
-  const initialValue = "let's start with this"
-
-  const TesContractFactory = await ethers.getContractFactory("TestContract")
-  const instance = await upgrades.deployProxy(TesContractFactory, [
-    initialValue,
-  ])
-
-  writeFileSync("./address", instance.address)
-
-  console.log(`deployed contract at ${instance.address}`)
+  // If you comment out the line below the gas reporter works fine.
+  ethers.utils.isAddress("0xFaKeAddrEss")
 }
 
 export default func
--- test/TestContract.test.ts
+++ test/TestContract.test.ts
@@ -1,34 +1,17 @@
 import { readFileSync } from "fs"
-import { ethers, deployments } from "hardhat"
+import { ethers, upgrades, deployments } from "hardhat"
 import { TestContract } from "../typechain-types"
 
 describe("TestContract", () => {
-  // it("test - upgradable", async () => {
-  //   const initialValue = "let's start with this"
-  //   const newValue = "surprise!"
-
-  //   const TesContractFactory = await ethers.getContractFactory("TestContract")
-  //   const testContractInstance = await upgrades.deployProxy(
-  //     TesContractFactory,
-  //     [initialValue]
-  //   )
-
-  //   await testContractInstance.setValue(newValue)
-  // })
-
-  it("test - fixture", async () => {
+  it("test - upgradable", async () => {
+    const initialValue = "let's start with this"
     const newValue = "surprise!"
 
-    await deployments.fixture()
-
-    const address = readFileSync("./address").toString()
-
-    const testContractInstance: TestContract = await ethers.getContractAt(
-      "TestContract",
-      address
+    const TesContractFactory = await ethers.getContractFactory("TestContract")
+    const testContractInstance = await upgrades.deployProxy(
+      TesContractFactory,
+      [initialValue]
     )
-
-    console.log(`in test use contract at ${testContractInstance.address}`)
 
     await testContractInstance.setValue(newValue)
   })
