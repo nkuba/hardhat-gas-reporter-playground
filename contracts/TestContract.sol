52bf76339277a6ca2cdb43f3744b32191a590195
Jakub Nowakowski <jakub.nowakowski@akena.co>
2022-05-03 08:25:43 +0200

    Remove reference to upgrades

--- contracts/TestContract.sol
+++ contracts/TestContract.sol
@@ -1,12 +1,10 @@
 pragma solidity ^0.8.9;
 
 
-import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
-
-contract TestContract is Initializable {
+contract TestContract {
     string storedValue;
 
-    function initialize(string memory initialValue) initializer public {
+    function initialize(string memory initialValue)  public {
         storedValue = initialValue;
     }
 
